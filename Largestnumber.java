06323cd337ebbc73b3b1b985b86ab85bb37e50ad
venkat-subrahmaniyan <vsb2903@gmail.com>
2016-05-06 08:26:39 +0530

    hunter1

---
+++ Largestnumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Largestnumber implements Comparator<Integer> {
+
+    public int compare(Integer o1, Integer o2) {
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+        return (s2+s1).compareTo(s1+s2);
+    }
+
+    public static void main(String[] args) {
+        Largestnumber ls = new Largestnumber();
+        Integer[] nums = {9,1,95,17,5};
+        Arrays.sort(nums, ls);
+        System.out.println(Arrays.toString(nums));
+    }
+}
